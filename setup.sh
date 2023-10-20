00dd4243e069e42a5ce6c693c5d5f811f9c5b73c
lihin929 <85750131+lihin929@users.noreply.github.com>
2023-10-20 10:02:02 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -19,25 +19,25 @@ CYAN='\033[0;36m'
 LIGHT='\033[0;37m'
 # ==========================================
 # Link Hosting Kalian Untuk Ssh Vpn
-wisnuvpn="raw.githubusercontent.com/inoyaksorojawi/large/sae/ssh"
+wisnuvpn="raw.githubusercontent.com/lihin929/allport443/sae/ssh"
 # Link Hosting Kalian Untuk Sstp
-wisnuvpnn="raw.githubusercontent.com/inoyaksorojawi/large/sae/sstp"
+wisnuvpnn="raw.githubusercontent.com/lihin929/allport443/sae/sstp"
 # Link Hosting Kalian Untuk Ssr
-wisnuvpnnn="raw.githubusercontent.com/inoyaksorojawi/large/sae/ssr"
+wisnuvpnnn="raw.githubusercontent.com/lihin929/allport443/sae/ssr"
 # Link Hosting Kalian Untuk Shadowsocks
-wisnuvpnnnn="raw.githubusercontent.com/inoyaksorojawi/large/sae/shadowsocks"
+wisnuvpnnnn="raw.githubusercontent.com/lihin929/allport443/sae/shadowsocks"
 # Link Hosting Kalian Untuk Wireguard
-wisnuvpnnnnn="raw.githubusercontent.com/inoyaksorojawi/large/sae/wireguard"
+wisnuvpnnnnn="raw.githubusercontent.com/lihin929/allport443/sae/wireguard"
 # Link Hosting Kalian Untuk Xray
-wisnuvpnnnnnn="raw.githubusercontent.com/inoyaksorojawi/large/sae/xray"
+wisnuvpnnnnnn="raw.githubusercontent.com/lihin929/allport443/sae/xray"
 # Link Hosting Kalian Untuk Ipsec
-wisnuvpnnnnnnn="raw.githubusercontent.com/inoyaksorojawi/large/sae/ipsec"
+wisnuvpnnnnnnn="raw.githubusercontent.com/lihin929/allport443/sae/ipsec"
 # Link Hosting Kalian Untuk Backup
-wisnuvpnnnnnnnn="raw.githubusercontent.com/inoyaksorojawi/large/sae/backup"
+wisnuvpnnnnnnnn="raw.githubusercontent.com/lihin929/allport443/sae/backup"
 # Link Hosting Kalian Untuk Websocket
-wisnuvpnnnnnnnnn="raw.githubusercontent.com/inoyaksorojawi/large/sae/websocket"
+wisnuvpnnnnnnnnn="raw.githubusercontent.com/lihin929/allport443/sae/websocket"
 # Link Hosting Kalian Untuk Ohp
-wisnuvpnnnnnnnnnn="raw.githubusercontent.com/inoyaksorojawi/large/sae/ohp"
+wisnuvpnnnnnnnnnn="raw.githubusercontent.com/lihin929/allport443/sae/ohp"
 
 # Getting
 MYIP=$(wget -qO- ipinfo.io/ip);
